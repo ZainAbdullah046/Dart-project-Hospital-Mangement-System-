c5ef5826e39e2b8e2a2dbb01a3f88bdf59a94216
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-02-15 19:44:22 +0500

    Home page

---
+++ Home.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  print("welcome to Hospital Management System");
+  String decision;
+  do {
+    print("PRESS 1 TO LOGIN AS OWNER");
+    print("PRESS 2 TO LOGIN AS EMPLOYEE");
+    print("PRESS 3 TO LOGIN AS DOCTOR");
+    print("PRESS 4 TO LOGIN AS PHARMACIST");
+    int choice = int.parse(stdin.readLineSync()!);
+    if (choice == 1) {
+      //owner();
+      print("hello O");
+    } else if (choice == 2) {
+      //employee();
+      print("hello E");
+    } else if (choice == 3) {
+      //doctor();
+      print("hello D");
+    } else if (choice == 4) {
+      //pharmacist();
+      print("hello P");
+    } else {
+      print("Invalid input! Please enter a valid option.");
+    }
+    print("PRESS 'T' TO TERMINATE AND 'C' TO CONTINUE");
+    decision = stdin.readLineSync()!;
+  } while (decision.toUpperCase() != 'T');
+}
